f2fcc09e913939323ca0d6d836332c4331c65a93
Ashish Kumar <ashishpurbey900@gamil.com>
2021-04-17 00:05:32 +0530

    complet Router and started checkout

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
 import './App.css';
 import Header from './Header'
 import Home from './Home'
+import {BrowserRouter as Router,Switch,Route} from 'react-router-dom'
+import Checkout from './Checkout'
 
 function App() {
   return (
-    <div className="app">
-       {/* Header */}
-       <Header/>
-       {/* Home  */}
-       <Home/> 
-    </div>
+    <Router>
+      <div className="app">
+        <Switch>
+          <Route path="/checkout">
+            <Header />
+            <Checkout />
+          
+          </Route>
+          <Route path="/">
+            <Header/>
+            <Home/> 
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
